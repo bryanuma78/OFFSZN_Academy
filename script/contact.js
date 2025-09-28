9bf287c959cb323312b8cd885cadfcf70f0589c4
jwolf <bradjoey7@gmail.com>
2025-09-27 19:07:44 -0500

    actualizacion

--- script/contact.js
+++ script/contact.js
@@ -1,17 +1,18 @@
-// Inicializar EmailJS
+// Inicializa EmailJS con tu Public Key
 emailjs.init("If_WAVcuXiGSPp2SB");
 
 const contactForm = document.getElementById("contactForm");
 
 contactForm.addEventListener("submit", function(e) {
-  e.preventDefault(); // Evita recarga de página
+  e.preventDefault();
 
-  emailjs.sendForm("service_w50l62y", "template_bgp3zb5", this)
+  emailjs.sendForm("service_w50l62y", "template_bgp3z", this)
     .then(() => {
-      alert("¡Mensaje enviado correctamente!");
-      contactForm.reset(); // Limpia los campos
-    }, (error) => {
-      console.error("Error al enviar:", error);
-      alert("Hubo un error al enviar el mensaje. Intenta de nuevo.");
+      alert("¡Mensaje enviado con éxito!");
+      contactForm.reset(); // limpia el formulario
+    })
+    .catch((error) => {
+      console.error("Error al enviar el mensaje:", error);
+      alert("Ocurrió un error. Intenta nuevamente.");
     });
 });
