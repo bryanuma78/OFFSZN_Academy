bc0add983bc211aa80d90c5d861010d06cda75d0
jwolf <bradjoey7@gmail.com>
2025-09-27 19:01:12 -0500

    actualizacion

--- script/contact.js
+++ script/contact.js
@@ -1,20 +1,16 @@
-// Inicializa EmailJS con tu Public Key
+// Inicializar EmailJS
 emailjs.init("If_WAVcuXiGSPp2SB");
 
-// Seleccionamos el formulario
 const contactForm = document.getElementById("contactForm");
 
-contactForm.addEventListener("submit", function (e) {
-  e.preventDefault(); // Evita que la página se recargue
+contactForm.addEventListener("submit", function(e) {
+  e.preventDefault(); // Evita recarga de página
 
-  // Envía el formulario usando EmailJS
   emailjs.sendForm("service_w50l62y", "template_bgp3zb5", this)
     .then(() => {
-      // Mensaje de éxito
       alert("¡Mensaje enviado correctamente!");
-      contactForm.reset(); // Limpia los campos del formulario
+      contactForm.reset(); // Limpia los campos
     }, (error) => {
-      // Mensaje de error
       console.error("Error al enviar:", error);
       alert("Hubo un error al enviar el mensaje. Intenta de nuevo.");
     });
