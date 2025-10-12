b73dd52161c25c7064e63f448ee030678a2e8e30
jwolf <bradjoey7@gmail.com>
2025-10-11 21:23:40 -0500

    actualizacion

---
+++ netlify/functions/payhip-proxy.js
@@ -0,0 +1,13 @@
+export async function handler(event, context) {
+    const res = await fetch("https://payhip.com/WillieInspiredbeats", {
+      headers: { "User-Agent": "Mozilla/5.0" }
+    });
+    const html = await res.text();
+  
+    return {
+      statusCode: 200,
+      headers: { "Content-Type": "text/html" },
+      body: html
+    };
+  }
+  
