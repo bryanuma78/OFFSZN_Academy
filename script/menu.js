d54e550b75d4b524c8e97d418367bdc5ce99bd6f
williebs64uwu-create <williebs64uwu@gmail.com>
2025-10-17 00:19:00 -0500

    Update menu.js

--- script/menu.js
+++ script/menu.js
@@ -1,13 +1,16 @@
 document.querySelector('.menu-toggle').onclick = function() {
   document.querySelector('nav#navMenu').classList.toggle('show');
+  document.body.classList.toggle('menu-open');
 };
 
 document.querySelector('.menu-close-btn').onclick = function() {
   document.querySelector('nav#navMenu').classList.remove('show');
+  document.body.classList.remove('menu-open');
 };
 
 document.querySelectorAll('nav#navMenu a').forEach(link => {
   link.onclick = function() {
     document.querySelector('nav#navMenu').classList.remove('show');
+    document.body.classList.remove('menu-open');
   };
 });
