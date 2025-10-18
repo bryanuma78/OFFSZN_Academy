5310dfde41f16dae1112595b39df8e693cb17b27
Juan Pablo Castañeda Ávila <juacastaneda@unal.edu.co>
2025-10-18 18:36:17 -0500

    Agregar páginas de autenticación y soporte CORS en el backend
    Se añadieron las páginas HTML de inicio de sesión, registro y panel de control, junto con sus respectivos archivos CSS y la lógica de autenticación en el frontend.
    Se integró el middleware CORS en el backend con Express para permitir solicitudes de origen cruzado.
    Se actualizaron las dependencias para incluir el paquete 'cors'.

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,13 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const { createClient } = require('@supabase/supabase-js');
 
 //configuracion app express
 const app = express();
 const port = process.env.PORT || 3001;
 app.use(express.json());
+app.use(cors());
 
 //configuracion supabase
 const supabase = createClient(
